a4dac33a585f76d909eedfcae4eff5f9962f532a
Astariul <remondnicola@gmail.com>
2021-02-28 19:20:22 +0900

    :pushpin: Update doctsring

--- agent.py
+++ agent.py
@@ -6,11 +6,24 @@ from module import Embedding, Encoder, Decoder, Critic
 
 class Agent(nn.Module):
     def __init__(self, space_dim=2, embed_hidden=128, enc_stacks=3, ff_hidden=512, enc_heads=16, query_hidden=360, att_hidden=256, crit_hidden=256, n_history=3, p_dropout=0.1):
-        """ Agent, made of an encoder + decoder for the actor part, and a critic
+        """Agent, made of an encoder + decoder for the actor part, and a critic
         part.
 
         Args:
-            input_embed (int, optional): Size of the encoded input. Defaults to 128.
+            space_dim (int, optional): Dimension for the cities coordinates.
+                Defaults to 2.
+            embed_hidden (int, optional): Embeddings hidden size. Defaults to 128.
+            enc_stacks (int, optional): Number of encoder layers. Defaults to 3.
+            ff_hidden (int, optional): Hidden size for the FF part of the encoder.
+                Defaults to 512.
+            enc_heads (int, optional): Number of attention heads for the encoder.
+                Defaults to 16.
+            query_hidden (int, optional): Query hidden size. Defaults to 360.
+            att_hidden (int, optional): Attention hidden size. Defaults to 256.
+            crit_hidden (int, optional): Critic hidden size. Defaults to 256.
+            n_history (int, optional): Size of history (memory size of the
+                decoder). Defaults to 3.
+            p_dropout (float, optional): Dropout rate. Defaults to 0.1.
         """
         super().__init__()
 
