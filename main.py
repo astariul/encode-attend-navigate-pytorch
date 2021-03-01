6668886dd0a30e1e53a7ef5068772ac163d8f7f8
Astariul <remondnicola@gmail.com>
2021-03-01 18:27:12 +0900

    :sparkles: Update main to run training and testing

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import wandb
 from omegaconf import OmegaConf as omg
+import torch
 
 from agent import Agent
-from trainer import Trainer
+from trainer import Trainer, reward_fn
+from data import DataGenerator
 
 
 def load_conf():
@@ -31,8 +33,42 @@ def main():
     agent = Agent(embed_hidden=conf.embed_hidden, enc_stacks=conf.enc_stacks, ff_hidden=conf.ff_hidden, enc_heads=conf.enc_heads, query_hidden=conf.query_hidden, att_hidden=conf.att_hidden, crit_hidden=conf.crit_hidden, n_history=conf.n_history, p_dropout=conf.p_dropout)
     wandb.watch(agent)
 
-    trainer = Trainer(conf, agent)
+    dataset = DataGenerator()
+
+    trainer = Trainer(conf, agent, dataset)
     trainer.run()
+
+    # Save trained agent
+    torch.save(agent.state_dict(), conf.model_path)
+
+    if conf.test:
+        # Load trained agent
+        agent.load_state_dict(torch.load(conf.model_path))
+        agent.eval()
+
+        running_reward = 0
+        for _ in range(conf.test_steps):
+            input_batch = dataset.test_batch(conf.batch_size, conf.max_len, conf.dimension, shuffle=False)
+
+            tour, *_ = agent(input_batch)
+
+            reward = reward_fn(input_batch, tour)
+
+            # Find best solution
+            j = reward.argmin()
+            best_tour = tour[j][:-1]
+
+            # Log
+            running_reward += reward[j]
+
+            # Display
+            print('Reward (before 2 opt)', reward[j])
+            opt_tour, opt_length = dataset.loop2opt(input_batch[0][best_tour])
+            print('Reward (with 2 opt)', opt_length)
+            dataset.visualize_2D_trip(opt_tour)
+
+        wandb.run.summary["test_reward"] = running_reward / conf.test_steps
+
 
 if __name__ == "__main__":
     main()
